bae8a703f323e2a2159d684c85e82a545b232b88
Steve Feehan <stevef00@pobox.com>
2022-07-25 19:30:21 -0400

    use ConfigParser
    Change config file to an .ini file and use ConfigParser. Add
    region_name to config file and get rid of unused username parameter.

--- ddbtest.py
+++ ddbtest.py
@@ -1,40 +1,43 @@
 #!/usr/bin/env python3
 
+from configparser import ConfigParser
 import boto3
 import boto3.session
-import re
 
-# FIXME: read keys from file
-# USERNAME=xxxx
-# ACCESS_KEY_ID=xxxxxx
-# SECRET_ACCESS_KEY=xxxxxx
+parser = ConfigParser()
+parser.read('config.ini')
 
-username = 'foo'
+#with open("keys.txt", "r") as keys_file:
+#    for line in keys_file:
+#        line = line.rstrip()
+#        if re.search("^#", line):
+#            continue
+#        elif re.search("^$", line):
+#            continue
 
-with open("keys.txt", "r") as keys_file:
-    for line in keys_file:
-        line = line.rstrip()
-        if re.search("^#", line):
-            continue
-        elif re.search("^$", line):
-            continue
+#        part = line.split('=')
+#        if part[0] == 'USERNAME':
+#            username = part[1]
+#        elif part[0] == 'ACCESS_KEY_ID':
+#            access_key_id = part[1]
+#        elif part[0] == 'SECRET_ACCESS_KEY':
+#            secret_access_key = part[1]
+#        elif part[0] == 'TABLE_NAME':
+#            table_name = part[1]
+#        else:
+#            print("ERROR")
 
-        part = line.split('=')
-        if part[0] == 'USERNAME':
-            username = part[1]
-        elif part[0] == 'ACCESS_KEY_ID':
-            access_key_id = part[1]
-        elif part[0] == 'SECRET_ACCESS_KEY':
-            secret_access_key = part[1]
-        elif part[0] == 'TABLE_NAME':
-            table_name = part[1]
-        else:
-            print("ERROR")
+region_name = parser.get('main', 'region_name')
+table_name = parser.get('main', 'table')
+access_key_id = parser.get('main', 'access_key_id')
+secret_access_key = parser.get('main', 'secret_access_key')
+
+# FIXME -- need to validate configuration
 
 dynamodb = boto3.resource('dynamodb',
         aws_access_key_id = access_key_id,
         aws_secret_access_key = secret_access_key,
-        region_name = 'us-east-1')
+        region_name = region_name)
 
 table = dynamodb.Table(table_name)
 

3251c6ce43728c8813b13664bba2bb95d416dc38
Steve Feehan <stevef00@pobox.com>
2022-07-26 07:50:14 -0400

    add some exception handling for configparser

--- ddbtest.py
+++ ddbtest.py
@@ -1,36 +1,21 @@
 #!/usr/bin/env python3
 
-from configparser import ConfigParser
+from configparser import ConfigParser, NoSectionError, NoOptionError
 import boto3
 import boto3.session
+import sys
 
 parser = ConfigParser()
 parser.read('config.ini')
 
-#with open("keys.txt", "r") as keys_file:
-#    for line in keys_file:
-#        line = line.rstrip()
-#        if re.search("^#", line):
-#            continue
-#        elif re.search("^$", line):
-#            continue
-
-#        part = line.split('=')
-#        if part[0] == 'USERNAME':
-#            username = part[1]
-#        elif part[0] == 'ACCESS_KEY_ID':
-#            access_key_id = part[1]
-#        elif part[0] == 'SECRET_ACCESS_KEY':
-#            secret_access_key = part[1]
-#        elif part[0] == 'TABLE_NAME':
-#            table_name = part[1]
-#        else:
-#            print("ERROR")
-
-region_name = parser.get('main', 'region_name')
-table_name = parser.get('main', 'table')
-access_key_id = parser.get('main', 'access_key_id')
-secret_access_key = parser.get('main', 'secret_access_key')
+try:
+    region_name = parser.get('main', 'region_name')
+    table_name = parser.get('main', 'table')
+    access_key_id = parser.get('main', 'access_key_id')
+    secret_access_key = parser.get('main', 'secret_access_key')
+except (NoSectionError, NoOptionError) as err:
+    print(f"Error reading config.ini: {err}")
+    sys.exit(1)
 
 # FIXME -- need to validate configuration
 

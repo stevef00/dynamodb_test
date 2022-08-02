99070a9cf6349c1daa09e9a7995e358b85eaea02
Steve Feehan <stevef00@pobox.com>
2022-08-02 10:04:02 -0400

    dddbtest.py: add some query, put and delete examples

--- ddbtest.py
+++ ddbtest.py
@@ -36,6 +36,27 @@ response = table.get_item(
 item = response['Item']
 print(item)
 
+table.put_item(
+   Item={
+        'userid': 'userid#0',
+        'bkmkid': 'bkmkid#3',
+        'url': 'https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html',
+    }
+)
+
+response = table.query(
+    KeyConditionExpression=Key('userid').eq('userid#0')
+)
+items = response['Items']
+print(items)
+
+table.delete_item(
+    Key={
+        'userid': 'userid#0',
+        'bkmkid': 'bkmkid#3'
+    }
+)
+
 response = table.query(
     KeyConditionExpression=Key('userid').eq('userid#0')
 )
